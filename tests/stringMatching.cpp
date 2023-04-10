eb436a6eb2ea568ace3d12bd00a8122b0fc2ff1e
fbrunodr <fbrunodr@gmail.com>
2023-04-10 11:22:36 -0300

    Added prefix function

---
+++ tests/stringMatching.cpp
@@ -0,0 +1,20 @@
+#include "../algorithms/prefixFunction.cpp"
+
+int main(){
+    string pattern, text;
+
+    while(getline(cin, pattern)){
+        getline(cin, text);
+        char separator = 5;
+        string aux = pattern;
+        aux.push_back(separator);
+        aux += text;
+        auto pi = prefixFunction(aux);
+        for(int i = pattern.size() + 1; i < pi.size(); i++)
+            if(pi[i] == pattern.size())
+                cout << i - 2*pattern.size() << ' ';
+        cout << endl;
+    }
+
+    return 0;
+}
