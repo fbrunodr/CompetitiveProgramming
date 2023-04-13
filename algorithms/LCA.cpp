12cf6b7ffd118c08bbc5ce67d5e01f944db2f43b
fbrunodr <fbrunodr@gmail.com>
2023-04-13 02:03:45 -0300

    Added Sparse Table and lowest common ancestor with sparse table

--- algorithms/LCA.cpp
+++ algorithms/LCA.cpp
@@ -1,6 +1,7 @@
 #include "../dataStructures/FrozenSegTree.cpp"
+#include "../dataStructures/SparseTable.cpp"
 
-class LCA{
+class SegLCA{
 
     using vi = vector<int>;
 
@@ -26,7 +27,7 @@ class LCA{
     }
 
     public:
-    LCA(const vector<vi>& AL_, int root){
+    SegLCA(const vector<vi>& AL_, int root){
         AL = AL_;
         n = AL.size();
 
@@ -51,6 +52,65 @@ class LCA{
         if(H[u] > H[v])
             swap(u, v);
         return E[segTree.RQ(H[u], H[v])];
+    }
+
+    int depth(int u){
+        return L[H[u]];
+    }
+};
+
+
+class SpTLCA{
+
+    using vi = vector<int>;
+
+    private:
+    int n;
+    vector<vi> AL;
+    vi L, E, H;
+    SparseTable<int> sparseTable;
+
+    int idx;
+    vector<bool> vis;
+    void dfs(int u, int depth){
+        vis[u] = true;
+        H[u] = idx;
+        E[idx] = u;
+        L[idx++] = depth;
+        for (auto &v : AL[u])
+            if(!vis[v]){
+                dfs(v, depth+1);
+                E[idx] = u; // backtrack to current node
+                L[idx++] = depth;
+            }
+    }
+
+    public:
+    SpTLCA(const vector<vi>& AL_, int root){
+        AL = AL_;
+        n = AL.size();
+
+        L = vi(2*n);
+        E = vi(2*n);
+        H = vi(n, -1);
+        idx = 0;
+        vis = vector<bool>(n, false);
+        dfs(root,0);
+
+        auto conquerer = [this](int i, int j){
+            return this->L[i] < this->L[j] ? i : j;
+        };
+
+        vi aux(2*n);
+        for(int i = 0; i < 2*n; i++)
+            aux[i] = i;
+        sparseTable = SparseTable<int>(aux, conquerer);
+    }
+
+    int lca(int u, int v){
+        if(H[u] > H[v])
+            swap(u, v);
+        return E[sparseTable.RQ(H[u], H[v])];
     }
 
     int depth(int u){
---
+++ dataStructures/SparseTable.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+class SparseTable{
+
+    using vi = vector<int>;
+    using vT = vector<T>;
+    using opT = function<T(T,T)>;
+
+private:
+    vT A;
+    vi P2, L2;
+    vector<vT> SpT; // the Sparse Table
+
+    opT conquerer;
+
+public:
+    SparseTable() {} // default constructor
+
+    SparseTable(vT &initialA, opT _conquerer) : conquerer(_conquerer) {
+        A = initialA;
+        int n = (int)A.size();
+        int L2_n = (int)log2(n) + 1;
+        P2.assign(L2_n + 1, 0);
+        L2.assign((1 << L2_n) + 1, 0);
+        for (int i = 0; i <= L2_n; ++i){
+            P2[i] = (1 << i); // to speed up 2^i
+            L2[(1 << i)] = i; // to speed up log_2(i)
+        }
+        for (int i = 2; i < P2[L2_n]; ++i)
+            if (L2[i] == 0)
+                L2[i] = L2[i - 1]; // to fill in the blanks
+
+        // the initialization phase
+        SpT = vector<vT>(L2[n]+1, vT(n));
+        for (int j = 0; j < n; ++j)
+            SpT[0][j] = A[j]; // RQ of sub array [j..j]
+
+        // the two nested loops below have overall time complexity = O(n log n)
+        for (int i = 1; P2[i] <= n; ++i) // for all i s.t. 2^i <= n
+            for (int j = 0; j + P2[i] - 1 < n; ++j){
+                T x = SpT[i - 1][j];             // [j..j+2^(i-1)-1]
+                T y = SpT[i - 1][j + P2[i - 1]]; // [j+2^(i-1)..j+2^i-1]
+                SpT[i][j] = conquerer(x, y);
+            }
+    }
+
+    T RQ(int i, int j){
+        int k = L2[j - i + 1];         // 2^k <= (j-i+1)
+        T x = SpT[k][i];               // covers [i..i+2^k-1]
+        T y = SpT[k][j - P2[k] + 1];   // covers [j-2^k+1..j]
+        return conquerer(x, y);
+    }
+};
