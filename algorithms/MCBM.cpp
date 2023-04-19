1bdd4cf4d4b6d286280b4f3eea3ab6dee6dd06e4
fbrunodr <fbrunodr@gmail.com>
2023-04-19 07:24:01 -0300

    Added Maximum Cardinality Bipartite Matching

---
+++ algorithms/MCBM.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class MCBM{
+
+    using vi = vector<int>;
+    using ii = pair<int, int>;
+    using vii = vector<ii>;
+
+private:
+    int V, VLeft;
+    vector<vi>& AL;
+
+    vi match, vis;
+
+    int Aug(int L) {
+        if (vis[L]) return 0;                          // L visited, return 0
+        vis[L] = 1;
+        for (auto &R : AL[L])
+            if((match[R] == -1) || Aug(match[R])) {
+                match[L] = R;                          // flip status
+                match[R] = L;
+                return 1;                              // found 1 matching
+            }
+
+        return 0;                                      // no matching
+    }
+
+public:
+
+    MCBM(vector<vi>& _AL, int _V, int _VLeft) : AL(_AL), V(_V), VLeft(_VLeft) {}
+
+    /**
+     * Returns MCBM and the matches
+    */
+    pair<int, vi> get(){
+        unordered_set<int> freeV;
+        for (int L = 0; L < VLeft; ++L)
+            freeV.insert(L);                             // initial assumption
+        match = vi(V, -1);
+        int MCBM = 0;
+        // Greedy pre-processing for trivial Augmenting Paths
+        // try commenting versus un-commenting this for-loop
+        for (int L = 0; L < VLeft; L++) {              // O(V+E)
+            vi candidates;
+            for (auto& R : AL[L])
+                if (match[R] == -1)
+                    candidates.push_back(R);
+            if ((int)candidates.size() > 0) {
+                ++MCBM;
+                freeV.erase(L);                            // L is matched
+                int a = rand()%(int)candidates.size();     // randomize this
+                int R = candidates[a];
+                match[L] = R;
+                match[R] = L;
+            }
+        }                                              // for each free vertex
+        for (auto& f : freeV) {                        // (in random order)
+            vis = vi(VLeft, 0);                        // reset first
+            MCBM += Aug(f);                            // try to match f
+        }
+
+        return {MCBM, match};
+    }
+
+};
---
+++ tests/flippingcards.cpp
@@ -0,0 +1,33 @@
+// see https://open.kattis.com/problems/flippingcards
+
+#include "../algorithms/MCBM.cpp"
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int T; cin >> T;
+
+    using vi = vector<int>;
+    vector<vi> AL;
+
+    while(T--){
+        int n; cin >> n;
+        int V = 3*n;
+        int VLeft = n;
+        AL = vector<vi>(V);
+
+        for(int card = 0; card < n; card++){
+            int numFront, numBack; cin >> numFront >> numBack;
+            numFront--;
+            numBack--;
+            AL[card].push_back(n+numFront);
+            AL[card].push_back(n+numBack);
+        }
+
+        MCBM solve(AL, V, VLeft);
+        auto [nMacthes, matches] = solve.get();
+
+        nMacthes == n ? cout << "possible" << endl : cout << "impossible" << endl;
+    }
+}
