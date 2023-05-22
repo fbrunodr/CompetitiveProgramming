26f63629fb1365c9bfa6b801c5aba1fe7ff3c5a4
fbrunodr <fbrunodr@gmail.com>
2023-05-22 00:04:17 -0300

    Added another test for next element to left/right

---
+++ tests/pairOfNumbers.cpp
@@ -0,0 +1,39 @@
+// see https://codeforces.com/problemset/problem/359/D
+
+#include "../algorithms/firstElementToLeft.cpp"
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    int n; cin >> n;
+
+    vi a(n);
+    for(auto& a_i : a)
+        cin >> a_i;
+
+    // opposite of not divisible is divisible
+    // divisible is transitive, that is:
+    // a|b and b|c => a|c
+    // so we can use next element to left on not divisible
+    auto notDivisible = [](int next, int curr){
+        return next%curr != 0;
+    };
+
+    vi nextLeft = firstElementToLeft<int>(a, notDivisible);
+    vi nextRight = firstElementToRight<int>(a, notDivisible);
+
+    int maxDist = -1;
+    for(int i = 0; i < n; i++)
+        maxDist = max(maxDist, (nextRight[i]-1) - (nextLeft[i]+1));
+
+    set<int> ans;
+    for(int i = 0; i < n; i++)
+        if((nextRight[i]-1) - (nextLeft[i]+1) == maxDist)
+            ans.insert(nextLeft[i]+2);
+
+    cout << ans.size() << ' ' << maxDist << '\n';
+    for(auto& i : ans)
+        cout << i << ' ';
+    cout << '\n';
+
+    return 0;
+}
