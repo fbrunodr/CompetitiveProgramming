53f1d6748914294f8bf36b23bc50c67be75aaf82
fbrunodr <fbrunodr@gmail.com>
2023-06-05 15:19:12 -0300

    Added more code to geometry library

--- algorithms/integerGeometry.cpp
+++ algorithms/integerGeometry.cpp
@@ -10,12 +10,26 @@ struct Point{
 
     i64 sqrMod() const { return (i64)x*x + (i64)y*y; }
 
-    bool operator == (Point other) const {
+    bool operator == (const Point& other) const {
         return x == other.x && y == other.y;
     }
 
-    bool operator != (Point other) const {
+    bool operator != (const Point& other) const {
         return !this->operator==(other);
+    }
+
+    // Points are ordered from left to right
+    // if tied, from bottom to top
+    bool operator < (const Point& other) const {
+        if(operator==(other))
+            return false;
+        if(x == other.x)
+            return y < other.y;
+        return x < other.x;
+    }
+
+    bool operator > (const Point& other) const {
+        return !operator==(other) && !operator<(other);
     }
 };
 
@@ -49,8 +63,12 @@ struct Vec{
         return !this->operator==(other);
     }
 
-    // for sorting acording to angle
+    // vectors are sorted according to their angle
     bool operator < (Vec other) const {
+        if(operator==(other))
+            return false;
+        if(x == 0 && y == 0) // (0,0) has priority
+            return true;
         if(top() != other.top())
             return top();
         auto css = cross(other);
@@ -110,5 +128,116 @@ class Angle{
 
     bool operator > (Angle other) const {
         return !operator==(other) && !operator<(other);
+    }
+};
+
+class Segment{
+    Point A, B;
+    Vec AB;
+
+    public:
+    Segment(Point _A, Point _B) : A(_A), B(_B) {
+        if(A == B)
+            exit(3708);
+        AB = Vec(A, B);
+    }
+
+    bool isCollinear(const Point& P) const {
+        return AB.cross(Vec(A, P)) == 0;
+    }
+
+    bool isBetween(const Point& P) const {
+        if(P == A)
+            return false;
+        if(P == B)
+            return false;
+
+        if(!isCollinear(P))
+            return false;
+        
+        Vec AP = Vec(A, P);
+        return AB.dot(AP) > 0 && AP.sqrMod() < AB.sqrMod();
+    }
+
+    bool isBetweenOrOn(const Point& P) const {
+        return (P == A) || (P == B) || isBetween(P);
+    }
+
+    bool isParallel(const Segment& other) const {
+        return AB.cross(other.AB) == 0;
+    }
+
+    bool isOnSameLine(const Segment& other) const {
+        return isParallel(other) && isCollinear(other.A);
+    }
+
+    // remember lines are infinity
+    bool doLinesCross(const Segment& other) const {
+        return !isParallel(other);
+    }
+
+    bool diffSideNotOnLine(const Point& P, const Point& Q) const {
+        i64 crossP = AB.cross(Vec(A, P));
+        if(crossP == 0)
+            return false;
+        i64 crossQ = AB.cross(Vec(A, Q));
+        if(crossQ == 0)
+            return false;
+        return signbit(crossP) != signbit(crossQ);
+    }
+
+    bool makeCross(const Segment& other) const {
+        return diffSideNotOnLine(other.A, other.B) && other.diffSideNotOnLine(A, B);
+    }
+};
+
+class Polygon{
+    vector<Point> points;
+
+    public:
+    Polygon() {}
+
+    Polygon(const vector<Point>& _points) : points(_points) {}
+
+    void push_back(const Point& P){
+        points.push_back(P);
+    }
+
+    bool isOnPolygon(const Point& P) const {
+        for(auto& point : points)
+            if(P == point)
+                return true;
+        auto aux = points;
+        aux.push_back(aux.front());
+        for(int i = 1; i < aux.size(); i++){
+            if(aux[i-1] == aux[i]) continue;
+            auto side = Segment(aux[i-1], aux[i]);
+            if(side.isBetween(P))
+                return true;
+        }
+
+        return false;
+    }
+
+    bool isInside(const Point& P) const {
+        if(points.size() < 3)
+            false;
+        if(isOnPolygon(P))
+            return false;
+        Point surelyOutside = P;
+        int delta = 1e9 + 5;
+        surelyOutside.x += delta;
+        surelyOutside.y += delta + 1;
+        Segment rayToOutside(P, surelyOutside);
+        auto aux = points;
+        aux.push_back(aux.front());
+        int crosses = 0;
+        for(int i = 1; i < aux.size(); i++){
+            if(aux[i-1] == aux[i])
+                continue;
+            if(rayToOutside.makeCross(Segment(aux[i-1], aux[i])))
+                crosses++;
+        }
+        return crosses & 1;
     }
 };
---
+++ tests/pointInPolygon.cpp
@@ -0,0 +1,35 @@
+// see https://open.kattis.com/problems/pointinpolygon
+
+#include "../algorithms/integerGeometry.cpp"
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n; cin >> n;
+
+    while(n){
+        Polygon polygon;
+        for(int i = 0; i < n; i++){
+            int x, y; cin >> x >> y;
+            polygon.push_back(Point(x, y));
+        }
+
+        int queries; cin >> queries;
+        while(queries--){
+            int x, y; cin >> x >> y;
+            Point P(x, y);
+
+            if(polygon.isOnPolygon(P))
+                cout << "on\n";
+            else if(polygon.isInside(P))
+                cout << "in\n";
+            else
+                cout <<"out\n";
+        }
+
+        cin >> n;
+    }
+
+    return 0;
+}
