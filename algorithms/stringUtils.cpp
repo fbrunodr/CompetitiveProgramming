e532d8317afbef427818244d9021968dfd9996fc
fbrunodr <fbrunodr@gmail.com>
2023-06-27 09:40:00 -0300

    fixing identation

--- algorithms/stringUtils.cpp
+++ algorithms/stringUtils.cpp
@@ -32,36 +32,36 @@ vi zFunction(const string& s) {
 }
 
 vi getSuffixArray(string s) {
-	s.push_back(5);
-	int n = s.size(), N = max(n, 260);
-	vi sa(n), ra(n);
-	for(int i = 0; i < n; i++) sa[i] = i, ra[i] = s[i];
+    s.push_back(5);
+    int n = s.size(), N = max(n, 260);
+    vi sa(n), ra(n);
+    for(int i = 0; i < n; i++) sa[i] = i, ra[i] = s[i];
 
-	for(int k = 0; k < n; k ? k *= 2 : k++) {
-		vi nsa(sa), nra(n), cnt(N);
+    for(int k = 0; k < n; k ? k *= 2 : k++) {
+        vi nsa(sa), nra(n), cnt(N);
 
-		for(int i = 0; i < n; i++) nsa[i] = (nsa[i]-k+n)%n, cnt[ra[i]]++;
-		for(int i = 1; i < N; i++) cnt[i] += cnt[i-1];
-		for(int i = n-1; i+1; i--) sa[--cnt[ra[nsa[i]]]] = nsa[i];
+        for(int i = 0; i < n; i++) nsa[i] = (nsa[i]-k+n)%n, cnt[ra[i]]++;
+        for(int i = 1; i < N; i++) cnt[i] += cnt[i-1];
+        for(int i = n-1; i+1; i--) sa[--cnt[ra[nsa[i]]]] = nsa[i];
 
-		for(int i = 1, r = 0; i < n; i++)
+        for(int i = 1, r = 0; i < n; i++)
             nra[sa[i]] = r += ra[sa[i]] != ra[sa[i-1]] or ra[(sa[i]+k)%n] != ra[(sa[i-1]+k)%n];
-		ra = nra;
-		if (ra[sa[n-1]] == n-1) break;
-	}
-	return vi(sa.begin()+1, sa.end());
+        ra = nra;
+        if (ra[sa[n-1]] == n-1) break;
+    }
+    return vi(sa.begin()+1, sa.end());
 }
 
 vi kasai(const string& s, const vi& sa) {
-	int n = s.size(), k = 0;
-	vi ra(n), lcp(n);
-	for (int i = 0; i < n; i++) ra[sa[i]] = i;
+    int n = s.size(), k = 0;
+    vi ra(n), lcp(n);
+    for (int i = 0; i < n; i++) ra[sa[i]] = i;
 
-	for (int i = 0; i < n; i++, k -= !!k) {
-		if (ra[i] == n-1) { k = 0; continue; }
-		int j = sa[ra[i]+1];
-		while (i+k < n and j+k < n and s[i+k] == s[j+k]) k++;
-		lcp[ra[i]] = k;
-	}
-	return lcp;
+    for (int i = 0; i < n; i++, k -= !!k) {
+        if (ra[i] == n-1) { k = 0; continue; }
+        int j = sa[ra[i]+1];
+        while (i+k < n and j+k < n and s[i+k] == s[j+k]) k++;
+        lcp[ra[i]] = k;
+    }
+    return lcp;
 }
