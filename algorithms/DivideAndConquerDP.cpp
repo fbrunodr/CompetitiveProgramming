b045ecb6ab991ad3776b95ac7dbba91a971aab40
Francisco Bruno Dias Ribeiro da Silva <fbrunodr@gmail.com>
2024-10-31 13:13:28 -0300

    Fixing trailing whitespaces

--- algorithms/DivideAndConquerDP.cpp
+++ algorithms/DivideAndConquerDP.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
+
 template<typename T>
 class DivideAndConquerDP{
- 
+
     using _T = optional<T>;
     using _T_T = pair<_T, _T>;
- 
+
 private:
     // [0, N-1]
     int N;
- 
+
     function<T(int)> singleElement;
     function<T(T&,int)> elementMerge;
- 
+
     vector<vector<_T>> memo;
- 
+
     void solveMoving(int mid, int d, bool toRight){
         if(memo[mid][d] != nullopt)
             return;
@@ -27,11 +27,11 @@ private:
         solveMoving(mid, d-1, toRight);
         memo[mid][d] = elementMerge(*memo[mid][d-1], idx);
     }
- 
+
     _T_T solve(int i, int j, int L, int R){
         int mid1 = (L + R) / 2;
         int mid2 = mid1 + 1;
- 
+
         if(j < mid1)
             return solve(i, j, L, mid1-1);
         else if(i > mid2)
@@ -49,25 +49,25 @@ private:
             solveMoving(mid2, j-mid2, true);
             return {memo[mid1][mid1-i], memo[mid2][j-mid2]};
         }
- 
+
         return {nullopt, nullopt};
     }
- 
+
     void init(int L, int R){
         if(L > R)
             return;
- 
+
         int mid1 = (L + R) / 2;
         int mid2 = mid1 + 1;
- 
+
         memo[mid1] = vector<_T>(mid1 - L + 1, nullopt);
         if(mid2 <= R)
             memo[mid2] = vector<_T>(R - mid2 + 1, nullopt);
- 
+
         init(L, mid1-1);
         init(mid2+1, R);
     }
- 
+
     public:
     DivideAndConquerDP(int _n, function<T(int)> _ele, function<T(T&,int)> _eleMerge){
         N = _n;
@@ -76,7 +76,7 @@ private:
         singleElement = _ele;
         elementMerge = _eleMerge;
     }
- 
+
     // The merge of intervals is left for the user
     _T_T RQ(int i, int j){
         if(i > j)
