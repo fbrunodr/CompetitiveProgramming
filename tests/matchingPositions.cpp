264c1b09fb15aa5d4769f8731e3b125b4e8e58ff
Francisco Bruno Dias Ribeiro da Silva <fbrunodr@Franciscos-MacBook-Pro.local>
2024-12-13 02:00:58 -0300

    Fixing bug on matching positions

---
+++ tests/matchingPositions.cpp
@@ -0,0 +1,16 @@
+#include "../algorithms/stringUtils.cpp"
+
+int main(){
+    string pattern = "hello";
+    string text = "hello world! hello is a word with four letters. Say hello too, please!";
+    for(int position : matching_positions(text, pattern, (char)5))
+        cout << position << ' ';
+    cout << endl;
+
+    vi pattern_numbers{1, 1, 1};
+    vi sequence{1, 1, 1, 1, 1, 1};
+    for(int position : matching_positions(sequence, pattern_numbers, (int)1e9))
+        cout << position << ' ';
+    cout << endl;
+    return 0;
+}
