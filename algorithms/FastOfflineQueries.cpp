5cb2c77de8d499ac6f5f3f9453c7a22a8a251f2b
Francisco Bruno Dias Ribeiro da Silva <fbrunodr@Franciscos-MacBook-Pro.local>
2024-12-22 14:52:45 -0300

    Using Mo's algorithm for fast offline queries

--- algorithms/FastOfflineQueries.cpp
+++ algorithms/FastOfflineQueries.cpp
@@ -8,42 +8,39 @@ using vii = vec<ii>;
 using i64 = long long;
 using iii = tuple<int, int, int>;
 
-// see https://en.wikipedia.org/wiki/Z-order_curve
-i64 interleave(int x, int y) {
-    i64 z = 0;
-    for (int i = 0; i < 32; ++i) {
-        z |= ((x >> i) & 1ULL) << (2 * i);
-        z |= ((y >> i) & 1ULL) << (2 * i + 1);
-    }
-    return z;
-}
-
+const int block_size = 512;
 
 template<typename D, typename T>
-vec<T> ZOrderSolveQueries(
+vec<T> MoSolveQueries(
     vii queries,
     function<void(D&,int)> add,
     function<void(D&,int)> remove,
     D& initialVal,
     function<T(D&)> save_answer
 ){
-    using i64iii = tuple<i64,int,int,int>;
+    using i4 = tuple<int,int,int,int>;
     int n = queries.size();
-    vec<i64iii> morton_query_order(n);
+    vec<i4> mo_queries(n);
     for(int i = 0; i < n; i++)
-        morton_query_order[i] = {
-            interleave(queries[i].first, queries[i].second),
-            queries[i].first,
+        mo_queries[i] = {
+            queries[i].first/block_size,
             queries[i].second,
+            queries[i].first,
             i
         };
 
-    sort(morton_query_order.begin(), morton_query_order.end());
+    sort(mo_queries.begin(), mo_queries.end(), [](i4 x, i4 y){
+        int block_x = get<0>(x);
+        int block_y = get<0>(y);
+        if(block_x == block_y)
+            return block_x & 1 ? get<1>(x) < get<1>(y) : get<1>(x) > get<1>(y);
+        return block_x < block_y;
+    });
 
     int begin = 0, end = 0;
     vec<T> ans(n);
     D& curr = initialVal;
-    for(auto [morton, l, r, i] : morton_query_order){
+    for(auto [block, r, l, i] : mo_queries){
         bool finished = false;
         while (!finished){
             if(begin > l){
