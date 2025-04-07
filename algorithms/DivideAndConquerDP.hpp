7a993f0a63c0377b803106c6147a0f54d41159af
Francisco Bruno Dias Ribeiro da Silva <fbrunodr@Franciscos-MacBook-Pro.local>
2025-04-07 00:13:56 -0300

    Reduced memory allocation of Divide and Conquer DP

--- algorithms/DivideAndConquerDP.hpp
+++ algorithms/DivideAndConquerDP.hpp
@@ -16,28 +16,38 @@ private:
     function<T(int)> singleElement;
     function<T(T&,int)> elementMerge;
 
-    vec<vec<_T>> memo;
+    int maxRecursionDepth;
+    vec<vec<T>> memo;
+    vec<vec<bool>> on;
 
     void solveMoving(int mid, int d, bool toRight){
-        if(memo[mid][d] != nullopt)
+        if(on[mid][d])
             return;
         int idx = toRight ? mid + d : mid - d;
+        on[mid][d] = true;
         if(d == 0){
             memo[mid][d] = singleElement(idx);
             return;
         }
         solveMoving(mid, d-1, toRight);
-        memo[mid][d] = elementMerge(*memo[mid][d-1], idx);
+        memo[mid][d] = elementMerge(memo[mid][d-1], idx);
     }
 
-    _T_T solve(int i, int j, int L, int R){
+    _T_T solve(int i, int j, int L, int R, int depth){
+        if(depth > maxRecursionDepth){
+            T ans = singleElement(i);
+            for(int idx = i+1; idx <= j; idx++)
+                ans = elementMerge(ans, idx);
+            return {ans, nullopt};
+        }
+
         int mid1 = (L + R) / 2;
         int mid2 = mid1 + 1;
 
         if(j < mid1)
-            return solve(i, j, L, mid1-1);
+            return solve(i, j, L, mid1-1, depth+1);
         else if(i > mid2)
-            return solve(i, j, mid2+1, R);
+            return solve(i, j, mid2+1, R, depth+1);
         else if(j == mid1){
             solveMoving(mid1, mid1-i, false);
             return {memo[mid1][mid1-i], nullopt};
@@ -55,26 +65,33 @@ private:
         return {nullopt, nullopt};
     }
 
-    void init(int L, int R){
+    void init(int L, int R, int depth){
         if(L > R)
+            return;
+        if(depth > maxRecursionDepth)
             return;
 
         int mid1 = (L + R) / 2;
         int mid2 = mid1 + 1;
 
-        memo[mid1] = vec<_T>(mid1 - L + 1, nullopt);
-        if(mid2 <= R)
-            memo[mid2] = vec<_T>(R - mid2 + 1, nullopt);
+        memo[mid1] = vec<T>(mid1 - L + 1);
+        on[mid1] = vec<bool>(mid1 - L + 1, false);
+        if(mid2 <= R){
+            memo[mid2] = vec<T>(R - mid2 + 1);
+            on[mid2] = vec<bool>(R - mid2 + 1, false);
+        }
 
-        init(L, mid1-1);
-        init(mid2+1, R);
+        init(L, mid1-1, depth+1);
+        init(mid2+1, R, depth+1);
     }
 
     public:
-    DivideAndConquerDP(int _n, function<T(int)> _ele, function<T(T&,int)> _eleMerge){
+    DivideAndConquerDP(int _n, function<T(int)> _ele, function<T(T&,int)> _eleMerge, int maxDepth = 15){
+        maxRecursionDepth = maxDepth;
         N = _n;
-        memo = vec<vec<_T>>(N);
-        init(0, N-1);
+        memo = vec<vec<T>>(N);
+        on = vec<vec<bool>>(N);
+        init(0, N-1, 1);
         singleElement = _ele;
         elementMerge = _eleMerge;
     }
@@ -83,7 +100,7 @@ private:
     _T_T RQ(int i, int j){
         if(i > j)
             exit(8742);
-        return solve(i, j, 0, N-1);
+        return solve(i, j, 0, N-1, 1);
     }
 };
 
--- tests/subsequenceSumQueries.cpp
+++ tests/subsequenceSumQueries.cpp
@@ -1,7 +1,6 @@
 // see https://codeforces.com/gym/101741/problem/J
-// latest submission: https://codeforces.com/gym/101741/submission/312404808
-// The geniuses from codeforces reduced memory requirements and bumped time by 5x
-// Not it is a completly distinct problem :smh:
+// latest submission: https://codeforces.com/gym/101741/submission/314310825
+// you have to remove the #define int long long int to not get MLE
 
 #include "../algorithms/DivideAndConquerDP.hpp"
 #define MOD (int)(1e9 + 7)
@@ -12,13 +11,11 @@ int32_t main(){
     int n, m;
     cin >> n >> m;
 
-    using vi = vector<int>;
     vi a(n);
     for(auto& a_i : a){
         cin >> a_i;
         a_i %= m;
     }
-
 
     auto element = [&](int i){
         vi ans(m, 0);
@@ -36,9 +33,7 @@ int32_t main(){
         return take;
     };
 
-    vi empty;
-
-    DivideAndConquerDP<vi> solver(n, element, mergeElement);
+    DivideAndConquerDP<vi> solver(n, element, mergeElement, 11);
 
     int queries; cin >> queries;
     while(queries--){
